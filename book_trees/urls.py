1ba4d1c6f387a7b3ffb62e9c0087ead1aadf74b3
JovelRamos <jovelmarkramos@gmail.com>
2026-02-22 21:21:27 -0800

    Add API endpoint to retrieve single Epub by id
    Add a new GET endpoint and view to fetch a single EpubFile by its id.

--- book_trees/urls.py
+++ book_trees/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('api/epubs/', views.epub_list_api),
+    path('api/epubs/<int:id>/', views.epub_api),
 ]
--- book_trees/views.py
+++ book_trees/views.py
@@ -1,10 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import EpubFile
 from .serializers import EpubSerializer
+
 
 @api_view(['GET'])
 def epub_list_api(request):
     epubs = EpubFile.objects.all()
     serializer = EpubSerializer(epubs, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def epub_api(request, id):
+    epub = get_object_or_404(EpubFile, id=id)
+    serializer = EpubSerializer(epub, many=False)
     return Response(serializer.data)
